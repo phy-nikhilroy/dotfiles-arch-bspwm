90547d1bf7e2da6290adb9a9688660ebad193b45
phy-nikhilroy <phy.nikhilroy@gmail.com>
2026-04-13 04:27:55 +0530

    initial arch setup for mb-pro a1278

---
+++ nvim/lua/plugins/fzf.lua
@@ -0,0 +1,30 @@
+return {
+	"ibhagwan/fzf-lua",
+	lazy = false,
+--	dependencies = { "nvim-tree/nvim-web-devicons" },
+	keys = {
+		{
+			"<leader>ff",
+			function()
+				require("fzf-lua").files()
+			end,
+			desc = "FZF Files",
+		},
+		{
+			"<leader>fg",
+			function()
+				require("fzf-lua").live_grep()
+			end,
+			desc = "FZF Live Grep",
+		},
+		{
+			"<leader>fb",
+			function()
+				require("fzf-lua").buffers()
+			end,
+			desc = "FZF Buffers",
+		},
+	},
+
+	opts = {},
+}
