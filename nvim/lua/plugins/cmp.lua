a83e11ac7a0b4f1dee479e0de033a7078afd83c7
phy-nikhilroy <phy.nikhilroy@gmail.com>
2026-05-09 03:45:37 +0530

    feat: make Tab/S-Tab select completion items when menu is visible

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -34,8 +34,19 @@ return {
                 }), 
                 -- Use Tab and Shift-Tab to jump through variables within a snippet (like an HTML boilerplate)
                 ["<Tab>"] = cmp.mapping(function(fallback)
-                    if luasnip.expand_or_jumpable() then
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif luasnip.expand_or_jumpable() then
                         luasnip.expand_or_jump()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
                     else
                         fallback()
                     end
